568b58ef6639a7184f744b9eb3ddad831aa99680
shibing624 <shibing624@126.com>
2024-06-26 16:44:11 +0800

    update idea demo.

--- examples/run_flow_demo.py
+++ examples/run_flow_demo.py
@@ -11,23 +11,24 @@ from actionflow.actionflow import Actionflow, Task
 from actionflow.tools.file import FileTool
 
 idea_assistant = Assistant(
-    name="brilliant entrepreneur",
+    name="企业家",
     llm=AzureOpenAILLM(model="gpt-4o"),
-    description="You are a brilliant entrepreneur. You are exceptional at generating new business ideas and marketing them.",
+    description="你是一位杰出的企业家。你擅长于产生新的商业创意并将其营销推广。",
     tools=[FileTool(data_dir="outputs")],
     output_dir="outputs",
     output_file_name="save.md",
 )
 
 workflow = Actionflow(
-    name="Brainstorm Workflow",
+    name="商业想法生成",
     tasks=[
         Task(
-            description="""1.Brainstorm five ideas for a education product.
-            2.Choose the best idea from the list based on likely purchase intent
-            3.Write a short description of the chosen product.
-            4.Create a simple HTML page with the chosen product, description, and list some benefits of the product in the table. 
-            5.Save the HTML page as 'product.html'.
+            description="""
+            1.提出五个教育产品创意。
+            2.根据可能的购买意图，从列表中选择最佳想法。
+            3.编写所选产品的简短描述。
+            4.创建一个简单的HTML页面，包括所选产品、描述，并在表格中列出该产品的一些优点。
+            5.将HTML页面保存为 'product.html'.
             """,
             assistant=idea_assistant,
         ),
