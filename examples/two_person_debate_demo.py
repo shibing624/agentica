293b039d4c80b911a1b504c269adc3884b90fbdc
shibing624 <shibing624@126.com>
2024-07-02 12:25:54 +0800

    update demo with debate.

--- examples/two_person_debate_demo.py
+++ examples/two_person_debate_demo.py
@@ -9,7 +9,7 @@ sys.path.append('..')
 from agentica import Assistant, OpenAILLM
 
 Biden = Assistant(
-    llm=OpenAILLM(model='gpt-4o'),
+    llm=OpenAILLM(model="gpt-3.5-turbo-1106"),
     name="Biden",
     description="Suppose you are Biden, you are in a debate with Trump.",
     show_tool_calls=True,
@@ -17,14 +17,14 @@ Biden = Assistant(
 )
 
 Trump = Assistant(
-    llm=OpenAILLM(model='gpt-4o'),
+    llm=OpenAILLM(model="gpt-3.5-turbo-1106"),
     name="Trump",
     description="Suppose you are Trump, you are in a debate with Biden.",
     show_tool_calls=True,
     debug_mode=True,
 )
 
-hn_assistant = Assistant(
+debate = Assistant(
     name="Debate",
     team=[Biden, Trump],
     instructions=[
@@ -36,7 +36,7 @@ hn_assistant = Assistant(
     output_file_name="debate.md",
     debug_mode=True,
 )
-hn_assistant.run(
+debate.run(
     """Trump and Biden are in a debate, Biden speak first, and then Trump speak, and then Biden speak, and so on, in 3 turns.
     Now begin."""
 )
