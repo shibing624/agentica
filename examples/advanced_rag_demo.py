9ff4fc75b5cec170a6ce1f5a52dfcbac1f0b0075
shibing624 <shibing624@126.com>
2024-07-02 22:20:28 +0800

    update rag.

--- examples/advanced_rag_demo.py
+++ examples/advanced_rag_demo.py
@@ -2,10 +2,16 @@
 """
 @author:XuMing(xuming624@qq.com)
 @description: Advanced RAG demo
+
 实现了高级版RAG，基于PDF文档回答问题，新增功能：pdf文件解析、query改写，字面+语义多路召回，召回排序（rerank）
+
+pip install rank_bm25 jieba agentica
 """
 import json
 import sys
+
+import jieba
+from rank_bm25 import BM25Okapi
 
 sys.path.append('..')
 from agentica import Assistant, AzureOpenAILLM
@@ -27,18 +33,22 @@ def merge_references_function(query: str, **kwargs) -> str:
     """Return a list of references from the knowledge base"""
     print(f"-*- Searching for references for query: {query}")
     # 向量检索
-    num_documents = 5
+    num_documents = 3
     emb_relevant_docs = knowledge_base.search(query=query, num_documents=num_documents)
+    print('emb_relevant_docs:', emb_relevant_docs)
     # 关键词检索
     all_documents = []
     for document_list in knowledge_base.document_lists:
         all_documents.extend(document_list)
-    keyword_relevant_docs = [doc for doc in all_documents if query.lower() in doc.content.lower()]
-    # 按单词出现的词频排序
-    keyword_relevant_docs = sorted(keyword_relevant_docs, key=lambda x: x.content.lower().count(query.lower()),
-                                   reverse=True)
+    tokenized_corpus = [jieba.lcut(doc.content) for doc in all_documents if doc]
+    bm25 = BM25Okapi(tokenized_corpus)
+    tokenized_query = jieba.lcut(query)
+    scores = bm25.get_scores(tokenized_query)
+    top_n_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:num_documents]
+    keyword_relevant_docs = [all_documents[i] for i in top_n_indices]
+    print('keyword_relevant_docs:', keyword_relevant_docs)
     # 合并两路召回结果
-    relevant_docs = emb_relevant_docs + keyword_relevant_docs[:num_documents]
+    relevant_docs = emb_relevant_docs + keyword_relevant_docs
     # 召回排序, todo: rerank
 
     if len(relevant_docs) == 0:
