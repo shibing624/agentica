bd4ebdefaa77636744149c5723342392f088d544
shibing624 <shibing624@126.com>
2024-09-29 12:03:54 +0800

    https://github.com/shibing624/agentica/issues/9

--- examples/advanced_rag_demo.py
+++ examples/advanced_rag_demo.py
@@ -5,13 +5,14 @@
 
 实现了高级版RAG，基于PDF文档回答问题，新增功能：pdf文件解析、query改写，字面+语义多路召回，召回排序（rerank）
 
-pip install similarities agentica
+pip install similarities agentica transformers torch
 """
-import json
 import sys
-
-import jieba
+import torch
+from typing import List, Union, Set
 from similarities import BM25Similarity
+from loguru import logger
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 sys.path.append('..')
 from agentica import Assistant, AzureOpenAILLM
@@ -19,11 +20,11 @@ from agentica.knowledge.knowledge_base import KnowledgeBase
 from agentica.vectordb.lancedb import LanceDb
 from agentica.emb.text2vec_emb import Text2VecEmb
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 knowledge_base = KnowledgeBase(
     data_path="data/paper_sample.pdf",
-    vector_db=LanceDb(
-        embedder=Text2VecEmb(),
-    ),
+    vector_db=LanceDb(embedder=Text2VecEmb())
 )
 # Load the knowledge base
 knowledge_base.load(recreate=False)
@@ -35,26 +36,55 @@ for document_list in knowledge_base.document_lists:
 corpus = [doc.content for doc in all_documents if doc]
 bm25_model = BM25Similarity(corpus)
 
+# Initialize rerank model
+rerank_model_name_or_path = "BAAI/bge-reranker-base"
+rerank_top_k = 3
+if rerank_model_name_or_path:
+    rerank_tokenizer = AutoTokenizer.from_pretrained(rerank_model_name_or_path)
+    rerank_model = AutoModelForSequenceClassification.from_pretrained(rerank_model_name_or_path)
+    rerank_model.to(device)
+    rerank_model.eval()
+    logger.info(f"rerank_model: {rerank_model}")
+
+
+def get_reranker_score(rerank_model, query: str, reference_results: Union[Set, List[str]]):
+    """Get reranker score."""
+    pairs = []
+    for reference in reference_results:
+        pairs.append([query, reference])
+    with torch.no_grad():
+        inputs = rerank_tokenizer(pairs, padding=True, truncation=True, return_tensors='pt', max_length=512)
+        inputs_on_device = {k: v.to(rerank_model.device) for k, v in inputs.items()}
+        scores = rerank_model(**inputs_on_device, return_dict=True).logits.view(-1, ).float()
+    return scores
+
 
 def merge_references_function(query: str, **kwargs) -> str:
     """Return a list of references from the knowledge base"""
-    print(f"-*- Searching for references for query: {query}")
+    logger.info(f"-*- Searching for references for query: {query}")
     # 向量检索
     num_documents = 3
     emb_res = knowledge_base.search(query=query, num_documents=num_documents)
     emb_relevant_docs = [i.content for i in emb_res]
-    print('emb_relevant_docs:', emb_relevant_docs)
+    logger.debug(f'emb_relevant_docs: {emb_relevant_docs}')
     # 关键词检索(基于BM25)
     res = bm25_model.search(query, num_documents)[0]
     keyword_relevant_docs = [i.get('corpus_doc') for i in res]
-    print('keyword_relevant_docs:', keyword_relevant_docs)
+    logger.debug(f'keyword_relevant_docs: {keyword_relevant_docs}')
     # 合并两路召回结果
-    relevant_docs = emb_relevant_docs + keyword_relevant_docs
-    # 召回排序, todo: reranker
+    relevant_docs = set(emb_relevant_docs + keyword_relevant_docs)
     if len(relevant_docs) == 0:
         return ""
+    # 召回排序，Rerank 模型排序
+    if rerank_model_name_or_path and rerank_model:
+        # Rerank reference results
+        rerank_scores = get_reranker_score(rerank_model, query, relevant_docs)
+        logger.debug(f"rerank_scores: {rerank_scores}")
+        # Get rerank top k chunks
+        relevant_docs = [reference for reference, score in sorted(
+            zip(relevant_docs, rerank_scores), key=lambda x: x[1], reverse=True)][:rerank_top_k]
     content = "\n".join(relevant_docs)
-    print('references:', content)
+    logger.info(f'references: {content}')
     return content
 
 
