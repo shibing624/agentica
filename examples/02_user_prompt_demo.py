f71fb8d9ce0f980edd5a4aee806501d6dcb8d702
shibing624 <shibing624@126.com>
2025-03-11 15:02:29 +0800

    update embs and support DeepSeek-R1

--- examples/02_user_prompt_demo.py
+++ examples/02_user_prompt_demo.py
@@ -2,17 +2,12 @@ import sys
 
 sys.path.append('..')
 from agentica import Agent
-from agentica import Moonshot, Qwen, OpenAIChat,Doubao
+from agentica import OpenAIChat
 
 m = Agent(
-    # model=Qwen(api_key='sk-8713fa90974545f5abf7464714d3b011', id='qwq-plus'),
-    # model=Doubao(api_key='e249abdf-51d4-482a-b5c8-8777a73990c2', id='ep-20250227113433-vv7hr'),
-    model=OpenAIChat(id='o3-mini'),
+    model=OpenAIChat(),
     system_prompt="分享一个两句话的故事。",
     user_prompt="12000年的爱。",
     debug_mode=True,
 )
-# r = m.run(stream=True)
-# for i in r:
-#     print(i)
-m.print_response(stream=True)
+m.print_response()
