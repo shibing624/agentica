823ad0081abc8d1f5b86af391692d44a60c2b3af
shibing624 <shibing624@126.com>
2025-05-29 21:04:52 +0800

    refactor: enhance memory handling in 04_memory_demo.py; add functionality to manage message history

--- examples/04_memory_demo.py
+++ examples/04_memory_demo.py
@@ -9,19 +9,39 @@ from agentica.memorydb import CsvMemoryDb
 
 m = Agent(
     model=OpenAIChat(),
-    memory=AgentMemory(db=CsvMemoryDb(csv_file_path='outputs/lisi.csv'), user_id="李四", create_user_memories=True),
-    add_history_to_messages=True,
-    debug_mode=True
+    add_history_to_messages=False,
+    debug_mode=False
 )
 
 # -*- Print a response
+r = m.run("李四住在北京，一家三口住大别墅")
+print(r)
+r = m.run("我前面问了啥")
+print(r)
+print("=====================")
+
+# add history to messages
+print("=== Add history to messages ===")
+m = Agent(
+    model=OpenAIChat(),
+    memory=AgentMemory(db=CsvMemoryDb(csv_file_path='outputs/test.csv'), user_id="李四", create_user_memories=True),
+    add_history_to_messages=True,
+    debug_mode=False
+)
+
+# -*- Print a response
+r = m.run("李四住在北京，一家三口住大别墅")
+print(r)
+r = m.run("我前面问了啥")
+print(r)
 r = m.run("李四住在北京，一家三口住大别墅，记住这个，你一句话介绍李四家庭情况")
 print(r)
 r = m.run("李四家里那边天气如何?是哪个气候带")
 print(r)
 r = m.run("介绍李四的情况")
 print(r)
-
+r = m.run("我前面问了啥")
+print(r)
 # -*- Get the memory
 memory = m.memory
 
