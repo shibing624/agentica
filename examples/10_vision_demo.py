0397db388a2d267b16772af91385258735f50786
shibing624 <shibing624@126.com>
2025-12-14 21:39:16 +0800

    update vision

--- examples/10_vision_demo.py
+++ examples/10_vision_demo.py
@@ -10,23 +10,28 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from agentica import Agent, OpenAIChat
 
-m = Agent(model=OpenAIChat(id="gpt-4o"))
+def main():
+    m = Agent(model=OpenAIChat(id="gpt-4o"))
 
-# Single Image
-r = m.run(
-    "描述图片",
-    images=[
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-    ]
-)
-print(r)
+    # Single Image
+    r = m.run(
+        "描述图片",
+        images=[
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+        ]
+    )
+    print(r)
 
-# Multiple Images
-r = m.run(
-    "一句话说明两张图片的不同",
-    images=[
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-        "https://img-blog.csdnimg.cn/img_convert/0ab31bdb17bebbab9c8f4185f3655b6d.jpeg",
-    ]
-)
-print(r)
+    # Multiple Images
+    r = m.run(
+        "一句话说明两张图片的不同",
+        images=[
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            "https://img-blog.csdnimg.cn/img_convert/0ab31bdb17bebbab9c8f4185f3655b6d.jpeg",
+        ]
+    )
+    print(r)
+
+if __name__ == "__main__":
+    main()
+    
